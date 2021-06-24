5740bb92b4ffd85e800c8c83ec61c28ad48641b3
davor <davorzec.emedia@gmail.com>
2021-06-24 09:32:25 +0200

    bugs

--- scrape.py
+++ scrape.py
@@ -25,8 +25,9 @@ timeouts = (6, 10) #connection timeout, read timeout
 retries = 1
 backoff_factor = 0.3
 
+#create proxies
 proxies.getProxies()
-
+"""
 #init db
 conn = mysql.connector.connect(
             host="localhost",
@@ -45,7 +46,7 @@ conn = mysql.connector.connect(
             charset="utf8mb4", 
             use_unicode=True
         )
-"""
+
 #get all locations
 locations_cursor = conn.cursor()
 locations_sql = "SELECT id, location_url FROM locations ORDER BY id DESC"
@@ -70,7 +71,7 @@ scrape_all_links = 0
 scrape_new_links = 0
 scrape_404_count = 0
 scrape_5xx_count = 0
-scrape_started_at = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+scrape_started_at = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
 
 for location in all_locations:
     scrape_location_time_start = time.time()
